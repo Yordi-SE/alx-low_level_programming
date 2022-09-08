f68ac5c7740518e2886f008c06bf20a56c4590bc
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-08 11:29:20 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
 	char i;
+
 	int k;
+
 	long int m;
+
 	long long int n;
+
 	float t;
+
 	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("size of an int: %d byte(s)\n", (unsigned)sizeof(k));
+	printf("size of an int: %d byte(s)\n", (unsigned int)sizeof(k));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(m));
-	printf("size of a long long int: %llu byte(s)\n", (long long unsigned)sizeof(n));
-	printf("size of a float: %1.0f byte(s)\n",(double)sizeof(t));
+	printf("size of a long long int: %llu byte(s)\n", (unsigned long long)
+			sizeof(n));
+	printf("size of a float: %1.0f byte(s)\n", (double)sizeof(t));
 	return (0);
 }
