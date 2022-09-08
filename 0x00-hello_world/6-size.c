52bc7f82e319abdcabad59fa45d2e0606836737d
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-08 12:15:39 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ int main(void)
 
 	float t;
 
-	printf("size of a char: %c byte(s)\n", (unsigned char)sizeof(i));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("size of an int: %d byte(s)\n", (unsigned int)sizeof(k));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(m));
 	printf("size of a long long int: %llu byte(s)\n", (unsigned long long)
 			sizeof(n));
-	printf("size of a float: %1.0f byte(s)\n", (double)sizeof(t));
+	printf("size of a float: %1.0f byte(s)\n", (float)sizeof(t));
 	return (0);
 }
