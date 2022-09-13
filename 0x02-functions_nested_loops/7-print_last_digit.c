caadfde9fdfd0c22094d44ecda54d6e06376089f
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-13 15:55:27 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,14 @@ int print_last_digit(int n)
 {
 	int result;
 
-	if (n < 0)
+	if (n < 0 && n >= -9)
+		result = (n * (-1));
+	if (n >= 0 && n <= 9)
+		result = n;
+	if (n < -9)
 		result = (n * (-1)) % 10;
-	if (n > 0)
+	if (n > 9)
 		result = n % 10;
-	if (n == 0)
-		result = 0;
 	_putchar(result + '0');
 	return (result);
 }
