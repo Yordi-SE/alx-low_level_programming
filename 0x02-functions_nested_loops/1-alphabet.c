c943c0e0b91c6813239e53f5f0cfee982c91a7a7
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-13 13:01:11 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Print lower case alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while
+		(c <= 'z') {
+			_putchar(c);
+			c++;
+		}
+	putchar('\n');
+}
