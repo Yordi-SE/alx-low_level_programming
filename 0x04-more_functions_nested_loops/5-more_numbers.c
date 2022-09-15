6a0a13b3d0ce23366dc0987b7c68115d3c71e809
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-15 11:20:43 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	int m;
+
+	int j;
+
+	int k;
+
+	int r;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+			_putchar(j + '0');
+		for
+
