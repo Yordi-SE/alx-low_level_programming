8054b9bd5045985ed643815b0d3ec8a3bbf083de
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-23 05:02:58 -0700

    hmm.. cool

--- 0x06-pointers_arrays_strings/compare_string.c
+++ 0x06-pointers_arrays_strings/compare_string.c
@@ -9,22 +9,42 @@ int _strcmp(char *s1, char *s2)
 {
 	int h;
 
+	int k;
+
 	int j;
 
 	int w[2];
 
 	int m = _strlen(s1);
 
+	int n = _strlen(s2);
+
 	int l = 0;
 
-	for (j = 0; j <= m; j++)
+	if (m < n)
 	{
-		if (s1[j] == s2[j])
-			h = 0;
-		else if (s1[j] != s2[j])
+		for (j = 0; j <= m; j++)
 		{
-			w[l] = j;
-			l = 1;
+			if (s1[j] == s2[j])
+				h = 0;
+			else if (s1[j] != s2[j])
+			{
+				w[l] = j;
+				l = 1;
+			}
+		}
+	}
+	if (m >= n)
+	{
+		for (k = 0; k <= n; k++)
+		{
+			if (s1[k] == s2[k])
+				h = 0;
+			else if (s1[k] != s2[k])
+			{
+				w[l] = k;
+				l = 1;
+			}
 		}
 	}
 	if (l == 1)
