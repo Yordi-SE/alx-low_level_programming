d741712169a27f06e7fc202fc8cad96a925ea696
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-22 18:45:54 -0700

    hmm.. so cool

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,18 @@ int _strcmp(char *s1, char *s2)
 {
 	int r;
 
-	if (s1[0] == s2[0])
+	int i;
+
+	int m = 0;
+
+	for (i = 0; (s1[i] != '\0') || (s2[i] != '\0'); i++)
+	{
+		if (s1[i] != s2[i])
+			m++;
+	}
+	if (m == 0)
 		r = 0;
-	else if ((s1[0] < s2[0]) || (s1[0] > s2[0]))
+	else
 		r = ((s1[0] - '0') - (s2[0] - '0'));
 	return (r);
 }
