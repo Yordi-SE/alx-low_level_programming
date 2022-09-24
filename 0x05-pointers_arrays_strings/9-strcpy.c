9360f109d9c3fb1fd0137b2164325b67682fa960
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-24 07:02:24 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,6 +7,47 @@
  * @src: src
  * Return: char
  */
-char *(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
+	int i;
 
+	int y = _strlen(src);
+
+	int x = _strlen(dest);
+
+	for (i = 0; i <= y; i++)
+	{
+		if (i > x)
+		{
+			x++;
+			dest[i] = src[i];
+		}
+		else
+			dest[i] = src[i];
+	}
+	return (dest);
+}
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: pointer to string
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 1;
+
+	int result = 0;
+
+	char r = *s;
+
+	while
+		(r != '\0') {
+		result++;
+		r = *(s + i);
+		i++;
+		}
+	return (result);
+}
