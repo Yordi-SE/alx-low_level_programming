36b2e41444987241111a9f84b029e15544d93313
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-09-28 14:17:42 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number.
+ * Return: int
+ * @n: integer
+ */
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -29,3 +29,4 @@ void _puts_recursion(char *s);
 #include <unistd.h>
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
