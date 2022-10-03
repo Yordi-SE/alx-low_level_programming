62161bc72d456d3208944699d980b7e9dd05ac34
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-03 16:57:23 -0700

    omg

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,6 @@ int **alloc_grid(int width, int height)
 	d = malloc(sizeof(m) * (height));
 	if (d == NULL)
 	{
-		for (i = 0; i < height; i++)
-			free(d[i]);
-		free(d);
 		return (NULL);
 	}
 	for (j = 0; j < height; j++)
@@ -31,6 +28,9 @@ int **alloc_grid(int width, int height)
 		m = malloc(sizeof(int) * (width));
 		if (m == NULL)
 		{
+			for (i = 0; i < j; i++)
+				free(d[i]);
+			free(d);
 			return (NULL);
 		}
 		d[j] = m;
