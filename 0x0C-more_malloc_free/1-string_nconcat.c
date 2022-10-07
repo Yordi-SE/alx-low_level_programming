d3c4f3ec8b53900e8d663270dc61d1214f97772b
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-07 03:36:34 -0700

    string nconat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,38 +12,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	char *f;
 
-	int r;
+	unsigned int i;
 
-	unsigned int i = _strlen(s2);
+	unsigned int m = _strlen(s2);
 
-	if (s1 == NULL && s2 == NULL)
+	if (n < m)
 	{
-		d = malloc(1);
-		return (d);
-	}
-	if (n < i)
-	{
-		if (s1 == NULL)
-			s1 = "";
-		if (s2 == NULL)
-			s2 = "";
 		f = malloc(sizeof(char) * n + 1);
 		for (i = 0; i < n; i++)
 			f[i] = s2[i];
 		f[i] = '\0';
-		r = _strlen(s2) - n;
-		d = malloc(_strlen(s1) + (_strlen(s2) - r) + 1);
-		if (d == NULL || f == NULL)
-			return (NULL);
 		d = str_concat(s1, f);
 	}
 	else
-	{
-		d = malloc(_strlen(s1) + _strlen(s2) + 1);
-		if (d == NULL)
-			return (NULL);
 		d = str_concat(s1, s2);
-	}
 	return (d);
 }
 /**
