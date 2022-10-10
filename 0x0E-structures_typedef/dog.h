4c8e7ace1cd46df4f33aec5d1216136b82fc6723
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-10 03:22:20 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct do - dog
+ * @name: first 
+ * @age: second
+ * @owner: owner
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
 
-#endif /*DOG_H*/
+#endif /* DOG_H */
