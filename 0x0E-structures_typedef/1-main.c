cad93572602a945382957ad1215c77f04822df6d
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-11 13:54:19 -0700

    print dog

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -8,9 +8,12 @@
  */
 int main(void)
 {
-    struct dog my_dog;
+    struct dog *my_dog;
+    struct dog dogg;
+    char *c = "cccaa";
+    my_dog = &dogg;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    init_dog(my_dog, c, 3.5, NULL);
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
 }
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "dog.h"
+/**
+ * print_dog - prints a stuct dog
+ * @d: pointer
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL || d->owner == NULL)
+		printf("nil\n");
+	else if (d == NULL)
+		return;
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
