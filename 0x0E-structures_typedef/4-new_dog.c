b46cb1015cf9eccdbb006845091a554452252497
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-12 02:42:21 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,5 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog.name = name;
 	new_dog.age = age;
 	new_dog.owner = owner;
+	if (new == NULL || new_dog.name == NULL)
+		return (NULL);
+	else if (new_dog.owner == NULL)
+		return (NULL);
 	return (new);
 }
