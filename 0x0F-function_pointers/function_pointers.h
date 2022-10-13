c7c5e611f627e0167b35bb14257c03e9a7e9bce6
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-13 06:50:42 -0700

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNC_POINTER
 #define FUNC_POINTER
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 
 #endif /*FUNC_POINTER*/
