855efa3f3240f9376045e802f8bdc021b48a935c
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-21 05:35:36 -0700

    list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	list_t *first;
 
-	first = malloc(sizeof(list_t));
+	first = (list_t *) malloc(sizeof(list_t));
 	if (first == NULL)
 		return (NULL);
 	first->next = *head;
