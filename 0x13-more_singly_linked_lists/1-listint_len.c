98a2af0472b7c28625b5659410d42bbc895f333b
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-24 09:25:09 -0700

    list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer
+ * Return: size_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while
+		(h != NULL) {
+			i++;
+			h = h->next;
+		}
+	return (i);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,6 +37,7 @@ size_t print_list(const list_t *h);
 size_t print_listint(const listint_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+size_t listint_len(const listint_t *h);
 int string_length(const char *pointer);
 list_t *add_node_end(list_t **head, const char *str);
 
