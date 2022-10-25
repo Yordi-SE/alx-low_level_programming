ea4093f7605236f5e9846e0ff7698686fa318d04
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-25 12:48:30 -0700

    list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to pointer
+ * @idx: index
+ * @n: int
+ * Return: pointer
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *s;
+
+	listint_t *m = *head;
+
+	listint_t *h;
+
+	listint_t *u;
+
+	s = malloc(sizeof(listint_t));
+	if (s == NULL)
+		return (NULL);
+	s->n = n;
+	if (*head == NULL)
+	{
+		*head = s;
+		return (*head);
+	}
+	h = get_nodeint_at_index(m, idx);
+	u = get_nodeint_at_index(m, idx - 1);
+	if (h == NULL)
+		return (NULL);
+	while
+		(m != u) {
+			m = m->next;
+		}
+	u->next = s;
+	s->next = h;
+	return (s);
+}
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer
+ * @index: index of the node
+ * Return: listint_t *
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	listint_t *s = head;
+
+	if (head == NULL)
+		return (NULL);
+	while
+		(i < index) {
+			if (s == NULL)
+				return (NULL);
+			s = s->next;
+			i++;
+		}
+	return (s);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -45,6 +45,7 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int string_length(const char *pointer);
 list_t *add_node_end(list_t **head, const char *str);
 
