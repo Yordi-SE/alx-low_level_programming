9bcc50ee0446568293807cc74fa1a8d7d8efa573
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-27 12:51:06 -0700

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/**
+ * binary_to_uint - convers a binary number to an unsigned int
+ * @b: character string
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = _strlen(b);
+
+	int j;
+
+	unsigned int h = 0;
+
+	int m = 0;
+
+	if (b == NULL)
+		return (0);
+	for (j = i - 1; j >= 0; j--)
+	{
+		if (b[j] == '0')
+		{
+			m++;
+		}
+		else if (b[j] == '1')
+		{
+			h = h + (mul(m));
+			m++;
+		}
+		else
+			return (0);
+	}
+	return (h);
+}
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: pointer to string
+ *
+ * Return: int
+ */
+int _strlen(const char *s)
+{
+	int i = 1;
+
+	int result = 0;
+
+	char r = *s;
+
+	while
+		(r != '\0') {
+		result++;
+		r = *(s + i);
+		i++;
+		}
+	return (result);
+}
+/**
+ * mul - power
+ * @n: number
+ * Return: int
+ */
+int mul(int n)
+{
+	int h = 2;
+
+	int i;
+
+	if (n == 0)
+		return (1);
+	for (i = 1; i < n; i++)
+		h *= 2;
+	return (h);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *b);
+int _strlen(const char *s);
+#include <stdio.h>
+#include <stdlib.h>
+int mul(int n);
+
+#endif /*MAIN_H*/
